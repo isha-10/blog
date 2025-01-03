9e95edd2a8843e7c3ff3104fb7f3f7106fe9ecaa
isha-10 <165405250+isha-10@users.noreply.github.com>
2025-01-03 02:25:42 -0800

    Initial commit from Create Next App

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/header'
+import Footer from './components/footer'
+import Hero from './components/hero'
+
+const Homepage = () => {
+  return (
+    <div>
+        <Hero />
+    </div>
+  )
+}
+
+export default Homepage
